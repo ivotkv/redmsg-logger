0d5c56ca3f4e184d4ca1c795f6001f6056c65c7e
Ivo Tzvetkov <ivotkv@gmail.com>
2017-08-09 10:29:15 -0400

    initial code structure

---
+++ redmsg_logger/handlers/sqlalchemy.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+# 
+# The MIT License (MIT)
+# 
+# Copyright (c) 2017 Ivo Tzvetkov
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+
+from __future__ import print_function, unicode_literals, absolute_import
+import sys
+import json
+from datetime import datetime
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.ext.declarative.api import DeclarativeMeta
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import create_engine
+from .base import BaseHandler
+from .exc import HandlerException
+
+class InvalidTableName(HandlerException):
+    pass
+
+class TableNotFound(HandlerException):
+    pass
+
+class SQLAlchemyDatabase(object):
+
+    def __init__(self, config):
+        self.url = '{0}://{1}:{2}@{3}/{4}'.format(config['type'],
+                                                  config['user'],
+                                                  config['pass'],
+                                                  config['host'],
+                                                  config['database'])
+        self.engine = create_engine(self.url, pool_recycle=3600)
+        self.automap = automap_base()
+        self.automap.prepare(self.engine, schema=config['schema'], reflect=True)
+        self.sessionmaker = sessionmaker(bind=self.engine, autoflush=False)
+
+    def session(self):
+        return self.sessionmaker()
+
+    def table(self, name):
+        table = getattr(self.automap.classes, name.lower(), None)
+        if table is not None and not isinstance(table, DeclarativeMeta):
+            raise InvalidTableName('reserved name cannot be a table name: {0}'.format(name))
+        return table
+
+class SQLAlchemyHandler(BaseHandler):
+
+    def __init__(self, config):
+        self.database = SQLAlchemyDatabase(config)
+
+    def get_latest_txid(self):
+        return None # TODO
+
+    def handle(self, message):
+        session = self.database.session()
+
+        try:
+            table = self.database.table(message['channel'])
+            if table is None:
+                raise TableNotFound('table does not exist or is not a viable SQLAlchemy table: {0}'.format(message['channel']))
+            data = json.loads(message['data'])
+            data['txid'] = message['txid']
+            session.add(table(**data['data']))
+        except Exception as e:
+            error = self.database.table('logger_errors')
+            if error is not None:
+                entry = session.add(error(date=str(datetime.now()),
+                                          remote_ip=self.client_address[0],
+                                          error=e.__class__.__name__,
+                                          description=str(e),
+                                          data=raw_data))
+            else:
+                sys.stderr.write('{0}: {1}: {2}\n'.format(e.__class__.__name__, e, raw_data).encode('utf-8'))
+
+        session.commit()
---
+++ redmsg_logger/logger.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# The MIT License (MIT)
+# 
+# Copyright (c) 2017 Ivo Tzvetkov
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+
+from __future__ import print_function, unicode_literals, absolute_import
+import sys
+from redmsg import Subscriber
+from handlers.sqlalchemy import SQLAlchemyHandler
+
+handlers = {
+    'sqlalchemy': SQLAlchemyHandler
+}
+
+class Logger(object):
+
+    def __init__(self, config):
+        self.channel = config['channel']
+        self.subscriber = Subscriber(**config['redmsg'])
+        self.handler = handlers[config['handler']](config[config['handler']])
+
+    def start(self):
+        self.subscriber.subscribe(self.channel)
+        latest_txid = self.handler.get_latest_txid(self.channel)
+        generator = self.subscriber.listen() if latest_txid is None else \
+                    self.subscriber.listen_from(latest_txid)
+        for message in generator:
+            try:
+                self.handler.handle(message)
+            except Exception as e:
+                sys.stderr.write('{0}: {1}: {2}\n'.format(e.__class__.__name__, e, message).encode('utf-8'))
+
+if __name__ == '__main__':
+    import yaml
+    from argparse import ArgumentParser
+    arg_parser = ArgumentParser()
+    arg_parser.description = 'RedMsg logging service.'
+    arg_parser.add_argument('--config', metavar='FILE', default='config.yaml',
+                            help='path to config file (default: %(default)s)')
+    args = arg_parser.parse_args()
+
+    with open(args.config, 'r') as file:
+        config = yaml.load(file)
+
+    logger = Logger(config)
+    logger.start()
